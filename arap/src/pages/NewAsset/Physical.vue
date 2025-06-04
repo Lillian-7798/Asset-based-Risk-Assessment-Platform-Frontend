5dc39a776c65ee99fffdf3537a82ca1f07f6de37
yuxuan lin <Lillian-7798@sjtu.edu.cn>
2025-06-04 19:56:19 +0800

    waiting for edit

--- arap/src/pages/NewAsset/Physical.vue
+++ arap/src/pages/NewAsset/Physical.vue
@@ -1,9 +1,65 @@
 <template>
-<div></div>
+    <el-row>
+        <el-col :span="1" />
+        <el-col :span="2" class="center-align">
+            <el-text class="q-text">Fixed Physical Asset?<span class="required-asterisk">*</span>:</el-text>
+        </el-col>
+        <el-col :span="7">
+            <el-select v-model="Fixed" placeholder="Default" style="width: 95%;justify-content: left; " clearable>
+                <el-option v-for="item in Fixed_op" :key="item.value" :label="item.label" :value="item.value" />
+            </el-select>
+        </el-col>
+    </el-row>
+
+    <!-- Fixed Pysical -->
+
+     <!-- Yes -->
+     <div v-if="Fixed === 'Yes'" class="conditional-content">
+        <el-row>
+            <el-col :span="1" />
+            <el-col :span="2" class="center-align">
+                <el-text class="q-text">Asset Category:</el-text>
+            </el-col>
+            <el-col :span="7">
+                <el-select v-model="LicenseType" placeholder="Default" style="width: 95%;justify-content: left; "
+                    clearable>
+                    <el-option v-for="item in LicenseTypes" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+            </el-col>
+            <el-col :span="2"></el-col>
+            <el-col :span="2" class="center-align">
+                <el-text class="q-text">License Number:</el-text>
+            </el-col>
+            <el-col :span="7">
+                <el-input style="width: 95%;justify-content: left;" v-model="input" placeholder="Please input"
+                    clearable />
+            </el-col>
+        </el-row>
+     </div>
+
+     <!-- No -->
+     <div v-if="Fixed === 'Yes'" class="conditional-content">
+
+     </div>
+
+     <el-divider class="divider" />
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            Fixed:"",
+            Fixed_op:[
+                {
+                    value:"Yes"
+                },
+                {
+                    value:"No"
+                }
+            ]
+        }
+    }
 }
 </script>
 
@@ -31,5 +87,10 @@ export default {
   margin: 0px auto;
   border-color: #e6903f;
   border-width: 2px
+}
+.conditional-content {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 }
 </style>
